233bc85b2db1141be331d23e5fe075f47e2dea73
jjw1179 <61392680+jjw1179@users.noreply.github.com>
2022-06-06 07:23:12 -0400

    Create ForLoop.py

---
+++ .github/workflows/ForLoop.py
@@ -0,0 +1,22 @@
+def driven_cost(Total):
+	return (float(d_miles) / m_per_gallon) * doll_per_gallon
+
+d_miles = float(input())
+m_per_gallon = float(input())
+doll_per_gallon = float(input())
+Total = '{:.2f}'.format(round((float(d_miles) / m_per_gallon) * doll_per_gallon,2))
+ 
+print(Total)
+
+d_miles = float(input())
+m_per_gallon = float(input())
+doll_per_gallon = float(input())
+Total = '{:.2f}'.format(round((float(d_miles) / m_per_gallon) * doll_per_gallon,2))
+
+print(Total)
+d_miles = float(input())
+m_per_gallon = float(input())
+doll_per_gallon = float(input())
+Total = '{:.2f}'.format(round((float(d_miles) / m_per_gallon) * doll_per_gallon,2))
+
+print(Total)
